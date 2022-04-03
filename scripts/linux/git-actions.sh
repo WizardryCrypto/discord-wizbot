23ae5a3dca8cd1d46ef0d716c622ea88b2ab2346
Blaque Allen <blaquewithaq@gmail.com>
2022-04-03 11:03:32 -0500

    Added git-actions.sh

---
+++ scripts/linux/git-actions.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+CURRENT_DIR=$(pwd)
+SUBMODULE="./wcData"
+LOGS_DIR="./logs"
+WHITELIST_DIR="./whitelist"
+
+# Git Push: discord-wizbot
+# Git Push: wcData
+
+# Git Pull: discord-wizbot
+function gitPullMaster() {
+    echo
+    echo "Git | Pull | Master"
+    git pull
+}
+
+# Git Pull: wcData
+function gitPullSubmodule() {
+    echo
+    echo "Git | Pull | wcData"
+    cd $SUBMODULE
+    git pull
+    cd $CURRENT_DIR
+}
+
+# Git Push: discord-wizbot
+function gitPushMaster() {
+    echo
+    echo "Git | Push | Master"
+    git Push
+}
+
+# Git Push: wcData
+function gitPushSubmodule() {
+    echo
+    echo "Git | Push | wcData"
+    cd $SUBMODULE
+    git Push
+    cd $CURRENT_DIR
+}
+
+# Git Monitor: wcData
+function gitMonitorSubmodule() {
+    echo
+    echo "Git | Monitor | wcData"
+    cd $SUBMODULE
+    inotifywait --exclude ".swp" -m -r -e CLOSE_WRITE \
+    --format="git add . && git commit -m 'Whitelist - Update' && git push origin discord-wizbot" \
+    "${WHITELIST_DIR}" | bash >> "$LOGS_DIR/monitor.txt" &
+}
+
+gitPullMaster
+gitPullSubmodule
+gitMonitorSubmodule
